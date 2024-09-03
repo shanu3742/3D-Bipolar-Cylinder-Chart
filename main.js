6a2e10cb0a36e6545ee89494fa64959451df0ea6
kumar <krksingh.99@gmail.com>
2024-09-04 02:30:27 +0530

    logic to show indicator line added

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ import { CylinderProgressBar } from "./src/data-visualization/component/Cylinder
 
 
 const main = () => {
+
   const graphArea = document.getElementById('graph-container');
   const width=  500;
   const height = 500;
   const dataToJoin = [1];
 
+
   const margin = {
     left:10,
     right:10,
     top:20,
-    bottom:20
+    bottom:100
   }
 
   const containerWidth = width-margin.left-margin.right;
@@ -45,7 +47,6 @@ const startingPointAlongX=20
 
 const path= `M${startingPointAlongX} ${containerHeight-groundHeight} L${containerWidth} ${containerHeight-groundHeight} L${containerWidth-startingPointAlongX} ${containerHeight} L${0} ${containerHeight} z`
 
-console.log(path)
 
 const axisWidth= containerWidth-startingPointAlongX*2;
 const xRange= [0+20,axisWidth-20];
@@ -93,9 +94,9 @@ graphContainer.selectAll('path#ground')
 const data = [
   {
     name:'TCS',
-    value:10,
-    positive:40,
-    negative:60
+    value:20,
+    positive:25,
+    negative:75
   },
   {
     name:'WIPRO',
@@ -114,7 +115,8 @@ const data = [
   value:30,
   positive:55,
   negative:45
-  }
+  },          
+               
 ]
 const xDomain = data.map((el) => el.name)
 const xScale = d3.scaleBand().domain([...xDomain]).range(xRange)
@@ -135,8 +137,13 @@ const yscale = d3.scaleLinear().domain(yDomain).range(yRange)
 console.log(xScale.bandwidth())
 const progressBar = CylinderProgressBar.getInstance();
 data.forEach((el,i) => {
-  progressBar.key(`bar-${el.name}-i`)
-  .select(graphContainer)
+  const indicatorContainer = graphContainer.selectAll(`g#indicator-${i}`)
+                               .data(dataToJoin)
+                               .join('g')
+                               .attr('id',`indicator-${i}`)
+
+  progressBar.key(`cylinder-${el.name}-i`)
+  .select(indicatorContainer)
   .x(xScale(el.name)+20+xScale.bandwidth()/4)
   .y(yscale(el.value))
   .height(containerHeight-20-yscale(el.value))
@@ -145,50 +152,68 @@ data.forEach((el,i) => {
   .on('click',() => console.log('second  click '))
   .draw()
 
-
-  graphContainer.append('rect')
-                .attr('x',xScale(el.name)+10+xScale.bandwidth()/4)
-                .attr('y',yscale(el.value))
-                .attr('height',(((containerHeight-20-yscale(el.value))*el.positive)/100)-2)
-                .attr('width','1')
-                .attr('fill','#f1addf')
-
-  
-  graphContainer.append('rect')
-                .attr('x',xScale(el.name)+10+xScale.bandwidth()/4)
-                .attr('y',(yscale(el.value)+(((containerHeight-20-yscale(el.value))*el.positive)/100))+2)
-                .attr('height',(((containerHeight-20-yscale(el.value))*el.negative)/100)-2)
-                .attr('width','1')
-                .attr('fill','#6B3D83')
+console.log(xScale.bandwidth())
+  if(el.value>5 && xScale.bandwidth()>50){
+    indicatorContainer.append('rect')
+    .attr('x',xScale(el.name)+12+xScale.bandwidth()/4)
+    .attr('y',yscale(el.value))
+    .attr('height',(((containerHeight-20-yscale(el.value))*el.positive)/100)-2)
+    .attr('width','1')
+    .attr('fill','#f1addf')
 
 
+    indicatorContainer.append('rect')
+    .attr('x',xScale(el.name)+12+xScale.bandwidth()/4)
+    .attr('y',(yscale(el.value)+(((containerHeight-20-yscale(el.value))*el.positive)/100))+2)
+    .attr('height',(((containerHeight-20-yscale(el.value))*el.negative)/100)-2)
+    .attr('width','1')
+    .attr('fill','#6B3D83')
+  }else{
+    //remove line
+  }
+ 
 
-                graphContainer.append("text")
-                .attr("x",(xScale(el.name)+0+xScale.bandwidth()/4)-30)
+if(xScale.bandwidth()>38){
+  // xScale(el.name)-xScale.bandwidth()/4
+  indicatorContainer.append("text")
+                .attr("x",xScale.bandwidth()>50?xScale(el.name)+10+xScale.bandwidth()/4:xScale(el.name)+xScale.bandwidth()/2)
                 .attr("y",yscale(el.value)+((((containerHeight-20-yscale(el.value))*el.positive)/100)-2)/2)
                 .attr("font-family", "Arial")
-                .attr("font-size",'16')
+                .attr("font-size",'9')
                 .attr("fill", "#f1addf")
+                .attr('text-anchor',xScale.bandwidth()>50?'end':'middle')
                 .text(`${el.positive}%`);
                 
-
-
-                graphContainer.append("text")
-                .attr("x",(xScale(el.name)+0+xScale.bandwidth()/4)-30)
+  indicatorContainer.append("text")
+                .attr("x",xScale.bandwidth()>50?xScale(el.name)+10+xScale.bandwidth()/4:xScale(el.name)+xScale.bandwidth()/2)
                 .attr("y",((yscale(el.value)+(((containerHeight-20-yscale(el.value))*el.positive)/100))+2)+((((containerHeight-20-yscale(el.value))*el.negative)/100)-2)/2)
                 .attr("font-family", "Arial")
-                .attr("font-size",'16')
-                .attr("fill", "#f1addf")
+                .attr("font-size",'9')
+                .attr("fill", "#6B3D83")
+                .attr('text-anchor',xScale.bandwidth()>50?'end':'middle')
                 .text(`${el.negative}%`);
+}else{
+  //remove text
+}
 
+const onTrunct  = (string) => {
+    if(string.length<=6){
+        return string
+      }else{
+        return string.substring(0,6)+'...'
+      }
+}
   // Create text and apply the shadow filter
-graphContainer.append("text")
-.attr("x",xScale(el.name)+20+xScale.bandwidth()/4)
-.attr("y",height-margin.bottom)
+graphContainer.append('g')
+.attr('transform',`translate(${xScale(el.name)+20+xScale.bandwidth()/4},${height-margin.bottom})`)
+.append("text")
+.attr("x",0)
+.attr("y",0)
 .attr("font-family", "Arial")
-.attr("font-size",'16')
+.attr("font-size",'12')
 .attr("fill", "steelblue")
-.text(`${el.name}:${el.value}`);
+.attr('transform',`translate(${xScale.bandwidth()/4},0) rotate(70)`)
+.text(`${onTrunct(el.name.toUpperCase())}:${el.value}`);
 })
 
 
